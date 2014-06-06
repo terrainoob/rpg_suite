a0b40281c47ba03f81407864a8862defac1e8e90
terrainoob <terrainoob@gmail.com>
2014-06-06 14:39:43 -0400

    add attribute definitions to size enums

--- app/enums/size.rb
+++ app/enums/size.rb
@@ -1,41 +1,166 @@
 class Size < ClassyEnum::Base
+  def initialize
+    @attributes = {}
+    @attributes[:mod] = 0
+    @attributes[:special_mod] = 0
+    @attributes[:fly_mod] = 0
+    @attributes[:stealth_mod] = 0
+    @attributes[:reach] = 0
+  end
+
+  def modifier
+    @attributes[:mod]
+  end
+
+  def special_modifier
+    @attributes[:special_mod]
+  end
+
+  def fly_modifier
+    @attributes[:fly_mod]
+  end
+
+  def stealth_modifier
+    @attributes[:stealth_mod]
+  end
+
+  def reach
+    @attributes[:reach]
+  end
 end
 
 class Size::Fine < Size
+  def initialize
+    super
+    @attributes[:mod] = 8
+    @attributes[:special_mod] = -8
+    @attributes[:fly_mod] = 8
+    @attributes[:stealth_mod] = 16
+  end
 end
 
 class Size::Diminutive < Size
+  def initialize
+    super
+    @attributes[:mod] = 4
+    @attributes[:special_mod] = -4
+    @attributes[:fly_mod] = 6
+    @attributes[:stealth_mod] = 12
+  end
 end
 
 class Size::Tiny < Size
+  def initialize
+    super
+    @attributes[:mod] = 2
+    @attributes[:special_mod] = -2
+    @attributes[:fly_mod] = 4
+    @attributes[:stealth_mod] = 8
+  end
 end
 
 class Size::Small < Size
+  def initialize
+    super
+    @attributes[:mod] = 1
+    @attributes[:special_mod] = -1
+    @attributes[:fly_mod] = 2
+    @attributes[:stealth_mod] = 4
+    @attributes[:reach] = 5
+  end
 end
 
 class Size::Medium < Size
+  def initialize
+    super
+    @attributes[:reach] = 5
+  end
 end
 
 class Size::LargeTall < Size
+  def initialize
+    super
+    @attributes[:mod] = -1
+    @attributes[:special_mod] = 1
+    @attributes[:fly_mod] = -2
+    @attributes[:stealth_mod] = -4
+    @attributes[:reach] = 10
+  end
 end
 
 class Size::LargeLong < Size
+  def initialize
+    super
+    @attributes[:mod] = -1
+    @attributes[:special_mod] = 1
+    @attributes[:fly_mod] = -2
+    @attributes[:stealth_mod] = -4
+    @attributes[:reach] = 10
+  end
 end
 
 class Size::HugeTall < Size
+  def initialize
+    super
+    @attributes[:mod] = -2
+    @attributes[:special_mod] = 2
+    @attributes[:fly_mod] = -4
+    @attributes[:stealth_mod] = -8
+    @attributes[:reach] = 15
+  end
 end
 
 class Size::HugeLong < Size
+  def initialize
+    super
+    @attributes[:mod] = -2
+    @attributes[:special_mod] = 2
+    @attributes[:fly_mod] = -4
+    @attributes[:stealth_mod] = -8
+    @attributes[:reach] = 15
+  end
 end
 
 class Size::GargantuanTall < Size
+  def initialize
+    super
+    @attributes[:mod] = -4
+    @attributes[:special_mod] = 4
+    @attributes[:fly_mod] = -6
+    @attributes[:stealth_mod] = -12
+    @attributes[:reach] = 20
+  end
 end
 
 class Size::GargantuanLong < Size
+  def initialize
+    super
+    @attributes[:mod] = -4
+    @attributes[:special_mod] = 4
+    @attributes[:fly_mod] = -6
+    @attributes[:stealth_mod] = -12
+    @attributes[:reach] = 20
+  end
 end
 
 class Size::ColossalTall < Size
+  def initialize
+    super
+    @attributes[:mod] = -8
+    @attributes[:special_mod] = 8
+    @attributes[:fly_mod] = -8
+    @attributes[:stealth_mod] = -16
+    @attributes[:reach] = 30
+  end
 end
 
 class Size::ColossalLong < Size
+  def initialize
+    super
+    @attributes[:mod] = -8
+    @attributes[:special_mod] = 8
+    @attributes[:fly_mod] = -8
+    @attributes[:stealth_mod] = -16
+    @attributes[:reach] = 30
+  end
 end
--- spec/enums/size_spec.rb
+++ spec/enums/size_spec.rb
@@ -1,53 +1,157 @@
 require 'spec_helper'
 
 describe Size::Fine do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::Fine.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq 8
+    expect(size.special_modifier).to eq -8
+    expect(size.fly_modifier).to eq 8
+    expect(size.stealth_modifier).to eq 16
+    expect(size.reach).to eq 0
+  end
 end
 
 describe Size::Diminutive do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::Diminutive.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq 4
+    expect(size.special_modifier).to eq -4
+    expect(size.fly_modifier).to eq 6
+    expect(size.stealth_modifier).to eq 12
+    expect(size.reach).to eq 0
+  end
 end
 
 describe Size::Tiny do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::Tiny.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq 2
+    expect(size.special_modifier).to eq -2
+    expect(size.fly_modifier).to eq 4
+    expect(size.stealth_modifier).to eq 8
+    expect(size.reach).to eq 0
+  end
 end
 
 describe Size::Small do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::Small.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq 1
+    expect(size.special_modifier).to eq -1
+    expect(size.fly_modifier).to eq 2
+    expect(size.stealth_modifier).to eq 4
+    expect(size.reach).to eq 5
+  end
 end
 
 describe Size::Medium do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::Medium.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq 0
+    expect(size.special_modifier).to eq 0
+    expect(size.fly_modifier).to eq 0
+    expect(size.stealth_modifier).to eq 0
+    expect(size.reach).to eq 5
+  end
 end
 
 describe Size::LargeTall do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::LargeTall.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -1
+    expect(size.special_modifier).to eq 1
+    expect(size.fly_modifier).to eq -2
+    expect(size.stealth_modifier).to eq -4
+    expect(size.reach).to eq 10
+  end
 end
 
 describe Size::LargeLong do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::LargeLong.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -1
+    expect(size.special_modifier).to eq 1
+    expect(size.fly_modifier).to eq -2
+    expect(size.stealth_modifier).to eq -4
+    expect(size.reach).to eq 10
+  end
 end
 
 describe Size::HugeTall do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::HugeTall.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -2
+    expect(size.special_modifier).to eq 2
+    expect(size.fly_modifier).to eq -4
+    expect(size.stealth_modifier).to eq -8
+    expect(size.reach).to eq 15
+  end
 end
 
 describe Size::HugeLong do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::HugeLong.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -2
+    expect(size.special_modifier).to eq 2
+    expect(size.fly_modifier).to eq -4
+    expect(size.stealth_modifier).to eq -8
+    expect(size.reach).to eq 15
+  end
 end
 
 describe Size::GargantuanTall do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::GargantuanTall.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -4
+    expect(size.special_modifier).to eq 4
+    expect(size.fly_modifier).to eq -6
+    expect(size.stealth_modifier).to eq -12
+    expect(size.reach).to eq 20
+  end
 end
 
 describe Size::GargantuanLong do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::GargantuanLong.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -4
+    expect(size.special_modifier).to eq 4
+    expect(size.fly_modifier).to eq -6
+    expect(size.stealth_modifier).to eq -12
+    expect(size.reach).to eq 20
+  end
 end
 
 describe Size::ColossalTall do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::ColossalTall.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -8
+    expect(size.special_modifier).to eq 8
+    expect(size.fly_modifier).to eq -8
+    expect(size.stealth_modifier).to eq -16
+    expect(size.reach).to eq 30
+  end
 end
 
 describe Size::ColossalLong do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:size) {Size::ColossalTall.new()}
+
+  it "has correctly set attributes" do
+    expect(size.modifier).to eq -8
+    expect(size.special_modifier).to eq 8
+    expect(size.fly_modifier).to eq -8
+    expect(size.stealth_modifier).to eq -16
+    expect(size.reach).to eq 30
+  end
 end
